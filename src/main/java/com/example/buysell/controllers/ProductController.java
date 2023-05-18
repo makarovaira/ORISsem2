21573d545c494f004786f8c8d320b7067aea72c4
qwertyyuuu <i9047691056@gmail.com>
2023-05-18 14:37:34 +0300

    add photo

--- src/main/java/com/example/buysell/controllers/ProductController.java
+++ src/main/java/com/example/buysell/controllers/ProductController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Controller
 @RequiredArgsConstructor
@@ -28,8 +31,9 @@ public class ProductController {
     }
 
     @PostMapping("/product/create")
-    public String createProduct(Product product) {
-        productService.saveProduct(product);
+    public String createProduct(@RequestParam("file1") MultipartFile file1, @RequestParam("file2") MultipartFile file2,
+                                @RequestParam("file3") MultipartFile file3, Product product) throws IOException, IOException {
+        productService.saveProduct(product, file1, file2, file3);
         return "redirect:/";
     }
 
